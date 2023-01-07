a2d6aa72fa9d7d3b7ff49eebebaf7aa6ad7cac18
Ankush <ankushtsalian@gmail.com>
2023-01-07 11:03:23 +0530

    created basic server express server

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('E-Commerce API');
+const express = require("express");
+const app = express();
+const port = process.env.Port || 5000;
+
+app.get("/api/v1", (req, res) => res.send("hello world"));
+
+app.listen(port, () => console.log(`listening at ${3000}`));

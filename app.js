13d9a9eb6100475eaf07d885427e3204f5694bff
Ankush <ankushtsalian@gmail.com>
2023-01-07 11:05:29 +0530

    created basic server express server

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
-const port = process.env.Port || 5000;
+
+const port = process.env.PORT || 5000;
 
 app.get("/api/v1", (req, res) => res.send("hello world"));
 
-app.listen(port, () => console.log(`listening at ${3000}`));
+const start = async () => {
+  try {
+    app.listen(port, () => console.log(`Server is listening at ${port}....`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+start();

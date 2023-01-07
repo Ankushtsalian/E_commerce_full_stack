ce7e9cf6fa18edc7be682f6ca35e309939f01bad
Ankush <ankushtsalian@gmail.com>
2023-01-07 11:22:27 +0530

    created basic server express server

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+require("./db/connect");
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
-
+const connectDB = require("./db/connect");
 const port = process.env.PORT || 5000;
 
 app.get("/api/v1", (req, res) => res.send("hello world"));
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`Server is listening at ${port}....`));
   } catch (error) {
     console.log(error);

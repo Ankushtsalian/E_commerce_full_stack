c039dbb3912be348af4f8082a54fe3b3e36d29b5
Ankush <ankushtsalian@gmail.com>
2023-01-07 11:40:06 +0530

    created basic server express errors handler using packages

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 require("./db/connect");
 require("dotenv").config();
+require("express-async-errors");
 
+// Express
 const express = require("express");
 const app = express();
+
+// Database
 const connectDB = require("./db/connect");
 const port = process.env.PORT || 5000;
 
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/not-found");
+
+// Middleware
+app.use(express.json());
+
 app.get("/api/v1", (req, res) => res.send("hello world"));
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
 
 const start = async () => {
   try {

a2c2bcad014978a8ac5e0214ce36a398f41f6796
Ankush <ankushtsalian@gmail.com>
2023-01-09 16:37:14 +0530

    ----------------------------PRODUCT ROUTE INITIATED-----------------------------------------------

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -3,6 +3,7 @@ const CustomError = require("../errors");
 const checkPermissions = (requestUser, resourceUserId) => {
   // Check for admin/user
   if (requestUser.role === "admin") return;
+
   //   Check if user is trying to access other users data except admin
   if (requestUser.userId === resourceUserId.toString()) return;
 

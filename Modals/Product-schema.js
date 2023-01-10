b4ec4d291c236c8e4a6698012bec73ea22aedef4
Ankush <ankushtsalian@gmail.com>
2023-01-10 13:59:26 +0530

    -----------------------------------------productSchema--------------------------------------------

--- Modals/Product-schema.js
+++ Modals/Product-schema.js
@@ -2,46 +2,69 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
-const productSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please Provide name"],
-    minlenth: 3,
-    maxlength: 50,
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "Please Provide name"],
+      maxlength: [100, "Name can not be more than 100 characters"],
+    },
+    price: {
+      type: String,
+      required: [true, "Please Provide Product Price"],
+      default: 0,
+    },
+    description: {
+      type: String,
+      required: [true, "Please Provide Product description"],
+      maxlength: [1000, "Description can not be more than 1000 characters"],
+    },
+    image: {
+      type: String,
+      default: "/uploads/example.jpeg",
+    },
+    category: {
+      type: String,
+      required: [true, "Please Provide Product category"],
+      enum: ["office", "kitchen", "bedroom"],
+    },
+    company: {
+      type: String,
+      required: [true, "Please Provide company"],
+      enum: {
+        values: ["ikea", "liddy", "marcos"],
+        message: "{VALUE} is not supported",
+      },
+    },
+    colors: {
+      type: [String],
+      required: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    freeShipping: {
+      type: Boolean,
+      default: false,
+    },
+    inventory: {
+      type: Number,
+      required: true,
+      default: 15,
+    },
+    averageRating: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  price: {
-    type: String,
-    required: [true, "Please Provide price"],
-  },
-  description: {
-    type: String,
-    required: [true, "Please Provide description"],
-    minlenth: 6,
-  },
-  image: {
-    type: String,
-  },
-  category: {
-    type: String,
-  },
-  company: {
-    type: String,
-  },
-  colors: {
-    type: [],
-  },
-  featured: {
-    type: Boolean,
-  },
-  freeShipping: {
-    type: Boolean,
-  },
-  inventory: {
-    type: Number,
-  },
-  averageRating: {
-    type: Number,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Product", productSchema);
